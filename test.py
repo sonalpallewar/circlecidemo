8f5e5d2da410d0a37cb5087413ef70fb653e1dd1
sonalpallewar <30591977+sonalpallewar@users.noreply.github.com>
2023-09-13 11:44:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print ("Hello Everyone from sonal")
+from time import sleep
+from app.celery_app import app
+from app.common.api_logging import logger
+from app.common.celery_client import handle_task_creation, handle_task_update
+
+
+@app.task(bind=True,max_retries=5,soft_time_limit=20)
+def taskonboardingservice(self, jwt_token: str):
+    response=""
+    try:
+        handle_task_creation(jwt_token, taskonboardingservice.request.id, taskonboardingservice.name)
+        sleep(5)
+        response = "Done response"
+        handle_task_update(jwt_token, taskonboardingservice.request.id, "SUCCESS")
+    except Exception:
+        logger.error("Error")
+        handle_task_update(jwt_token, taskonboardingservice.request.id, "FAILED")
+        response = "bad response"
+    return response

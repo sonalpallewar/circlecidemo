11f6f6f48c5380e155b89528f0d486e225e1c584
sonalpallewar <30591977+sonalpallewar@users.noreply.github.com>
2023-09-13 11:45:21 +0530

    Update test3.py

--- circle/ci/test/test3.py
+++ circle/ci/test/test3.py
@@ -1,1 +1,7 @@
-print("test3")
+from celery import Celery
+from app.common.env_config import settings
+
+app = Celery("onboardingservice", broker=settings.REDIS_URL+"/10", 
+                     backend=settings.REDIS_URL_RESULT+"/11",
+                     broker_connection_retry_on_startup=True,
+                     include=['app.task'])

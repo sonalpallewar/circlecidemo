dc67c73830436c9825f3a24faa7630e1f585301b
sonalpallewar <30591977+sonalpallewar@users.noreply.github.com>
2024-03-01 17:13:41 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -5,3 +5,5 @@ app = Celery("onboardingservice", broker=settings.REDIS_URL+"/10",
                      backend=settings.REDIS_URL_RESULT+"/11",
                      broker_connection_retry_on_startup=True,
                      include=['app.task'])
+def prits():
+  print("Hello sonal here")
